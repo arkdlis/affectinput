19d8295056fa29510b6ef096320d4c72a98fe9db
Arek <vulpes.lis@gmail.com>
2018-12-05 22:41:45 +0100

    valence-arousal

--- dev/valence-arousal/script.js
+++ dev/valence-arousal/script.js
@@ -1,173 +1,206 @@
 var sketch = function(p) {
 
-  var canvasWidth = 400;
+  var subjectives = [
+    'Excited',
+    'Delighted',
+    'Happy',
+    'Content',
+    'Relaxed',
+    'Calm',
+    'Tired',
+    'Bored',
+    'Depressed',
+    'Frustrated',
+    'Angry',
+    'Tense'
+  ];
 
-  if (window.innerWidth < 400) {
+  var smallScreen = false;
+
+  var canvasWidth = 500;
+  if (window.innerWidth < canvasWidth) {
+    smallScreen = true;
     canvasWidth = window.innerWidth;
   }
+  var canvasHeight = canvasWidth + (smallScreen ? 60 : 100);
 
-  var canvasHeight = 60;
+  var cursorSize = 16;
+  var helpRadius = smallScreen ? 40 : 70;
 
-  var strokeBold = 35;
-  var strokeRoot = 20;
-  var strokeSpring = 5;
-  var strokeLight = 5;
-  var darkGrey;
+  var strokeBold = 25;
+  var strokeLight = 2;
+  var dark;
   var mediumGrey;
-  var lightGrey;
+  var lightBlue;
 
-  var springPadding = 40;
-  var enableSpring = true;
+  var enableHints = false;
 
   var padding = {
     x: 10,
     y: 10,
   }
 
-  var barWidth = canvasWidth - 2*padding.x - strokeBold;
-  var handleWidth = 20;
-
-  var position = springPadding;
-  var lastPosition = position;
-  var velocity = 0;
-  var acceleration = 0;
-
-  var springFactor = 0.15;
-  var frictionFactor = 0.22;
+  var position = {
+    x: canvasWidth/2,
+    y: canvasWidth/2
+  };
 
   var pdragged = false;
 
   p.setup = function() {
     p.createCanvas(canvasWidth, canvasHeight);
 
-    darkGrey = p.color(190, 190, 190);
-    mediumGrey = p.color(220, 220, 220);
-    lightGrey = p.color(235, 235, 235);
+    dark = p.color(29, 29, 29);
+    mediumGrey = p.color(173, 173, 173);
+    lightBlue = p.color(231, 238, 248);
   }
 
-  function clampPosition(position) {
-    if (position < springPadding) {
-      return springPadding;
-    }
-    if (position > barWidth - handleWidth) {
-      return barWidth - handleWidth;
-    }
-    return position;
-  }
-
-  function mouseOnHandle(mouseX, mouseY, position) {
-    if (mouseX >= position + padding.x && mouseX <= position + padding.x + handleWidth + strokeBold &&
-      mouseY >= padding.y && mouseY <= padding.y + strokeBold) {
+  function mouseInVASpace(mouseX, mouseY) {
+    if (mouseX >= padding.x && mouseX <= canvasWidth - padding.x && 
+        mouseY >= padding.y && mouseY <= canvasWidth - padding.y){
       return true;
     }
     return false;
   }
 
-  function drawSpring(position, steps) {
-    rootX = padding.x + strokeRoot/2;
-    rootY = padding.y;
-    let stepWidth = position/steps;
-    // root
-    p.stroke(mediumGrey);
-    p.strokeWeight(strokeRoot);
+  function mouseInCircle(mouseX, mouseY, x, y, radius) {
+    let dpx = Math.pow(mouseX - x, 2);
+    let dpy = Math.pow(mouseY - y, 2);
+    let rp = Math.pow(radius, 2);
+    if (dpx + dpy < rp) {
+      return true;
+    }
+    return false;
+  }
+
+  function drawLabels(text, x, y) {
+    let backfaceLength = (smallScreen ? 20 : 35)
+    p.textSize((smallScreen ? 12 : 16));
+    p.textAlign(p.CENTER, p.CENTER);
+    p.fill(mediumGrey);
+    p.stroke(lightBlue);
+    p.strokeWeight(strokeBold);
     p.line(
-      rootX, rootY + strokeRoot/2, 
-      rootX, rootY + strokeBold - strokeRoot/2
+      x - backfaceLength, y,
+      x + backfaceLength, y,
     );
-    // spring
-    p.stroke(mediumGrey);
-    p.strokeWeight(strokeSpring);
-    for (let i = 0; i < steps; i++) {
-      let x = rootX + stepWidth*i;
-      let y = i%2 ? rootY + strokeSpring/2 : rootY + strokeBold - strokeSpring/2;
-      let nx = rootX + stepWidth*(i+1);
-      let ny = (i+1)%2 ? rootY + strokeSpring/2 : rootY + strokeBold - strokeSpring/2;
-      p.line(
-        x, y, 
-        nx, ny
-      );
+    p.noStroke();
+    p.text(text, x, y);
+  }
+
+  function drawHints(text, x, y) {
+    p.textSize((smallScreen ? 10 : 16));
+    p.textAlign(p.CENTER, p.CENTER);
+    p.fill(dark);
+    p.noStroke();
+    p.text(text, x, y);
+  }
+
+  p.mouseClicked = function() {
+    if (mouseInCircle(p.mouseX, p.mouseY, canvasWidth - helpRadius/2 - padding.x, canvasHeight - helpRadius/2 - padding.y, helpRadius/2)) {
+      enableHints = !enableHints;
     }
   }
 
   p.draw = function() {
     p.clear();
-    if (mouseOnHandle(p.mouseX, p.mouseY, position) || pdragged) {
-      p.cursor(p.HAND);
+    if (mouseInVASpace(p.mouseX, p.mouseY) || pdragged) {
+      p.cursor(p.CROSS);
       if (p.mouseIsPressed) {
-        pdragged = true;
-        position = clampPosition(p.mouseX - padding.x - strokeBold/2 - handleWidth/2);
-      } else {
-        lastPosition = pdragged ? position : lastPosition;
-        pdragged = false;
+        position = {
+          x: p.mouseX,
+          y: p.mouseY
+        };
+        pdragged
       }
     } else {
       p.cursor(p.ARROW);
-      pdragged = false;
     }
-    if (!p.mouseIsPressed) {
-      acceleration = -springFactor*(position - springPadding);
-      velocity += acceleration - frictionFactor*velocity;
-      if (position < 0) {
-        position = 0;
-        velocity = -velocity;
-      }
-      if (velocity < 0.5 && Math.abs(position) < 1) {
-        acceleration = 0;
-        velocity = 0;
-      }
-      position += velocity;
-    } else {
-      acceleration = 0;
-      velocity = 0;
+    if (mouseInCircle(p.mouseX, p.mouseY, canvasWidth - helpRadius/2 - padding.x, canvasHeight - helpRadius/2 - padding.y, helpRadius/2)) {
+      p.cursor(p.HAND);
     }
-    // last position
-    p.fill(lightGrey);
-    p.strokeWeight(0);
-    p.triangle(
-      strokeBold/2 + padding.x + springPadding + handleWidth/2, padding.y + strokeBold,
-      strokeBold/2 + padding.x + springPadding + handleWidth/2 + 4, padding.y + strokeBold + 9,
-      strokeBold/2 + padding.x + springPadding + handleWidth/2 - 4, padding.y + strokeBold + 9
-    );
-    p.fill(darkGrey);
-    p.strokeWeight(0);
-    p.triangle(
-      strokeBold/2 + padding.x + lastPosition + handleWidth/2, padding.y + strokeBold,
-      strokeBold/2 + padding.x + lastPosition + handleWidth/2 + 4, padding.y + strokeBold + 9,
-      strokeBold/2 + padding.x + lastPosition + handleWidth/2 - 4, padding.y + strokeBold + 9
-    );
-    //bar
-    p.stroke(lightGrey);
-    p.strokeWeight(strokeBold);
-    p.line(
-      strokeBold/2 + padding.x, strokeBold/2 + padding.y, 
-      canvasWidth - padding.x - strokeBold/2, strokeBold/2 + padding.y
-    );
-    // spring
-    if (enableSpring) {
-      drawSpring(position, 8)
-    }
-    // handle
-    p.stroke(darkGrey);
-    p.strokeWeight(strokeBold);
-    p.line(
-      strokeBold/2 + padding.x + position, strokeBold/2 + padding.y, 
-      strokeBold/2 + padding.x + position + handleWidth, strokeBold/2 + padding.y
-    );
-    p.stroke(lightGrey);
+    // background circle
+    p.noStroke();
+    p.fill(lightBlue);
+    p.ellipse(canvasWidth/2, canvasWidth/2, canvasWidth - padding.x*2 - 40, canvasWidth - padding.y*2 - 40);
+    // axis
+    p.stroke(mediumGrey);
     p.strokeWeight(strokeLight);
     p.line(
-      strokeBold/2 + padding.x + position + handleWidth/2, strokeLight/2 + padding.y + strokeBold/5, 
-      strokeBold/2 + padding.x + position + handleWidth/2, padding.y + strokeBold - strokeLight/2 - strokeBold/5
+      canvasWidth/2, padding.y + 10,
+      canvasWidth/2, canvasWidth - padding.y -10
     );
     p.line(
-      strokeBold/2 + padding.x + position, strokeLight/2 + padding.y + strokeBold/5, 
-      strokeBold/2 + padding.x + position, padding.y + strokeBold - strokeLight/2 - strokeBold/5
+      padding.x + 10, canvasWidth/2,
+      canvasWidth - padding.x - 10, canvasWidth/2
+    );
+    // upper arrow
+    p.stroke(mediumGrey);
+    p.fill(mediumGrey);
+    p.triangle(
+      canvasWidth/2, padding.y,
+      canvasWidth/2 + 5, padding.y + 12,
+      canvasWidth/2 - 5, padding.y + 12,
+    )
+    // lower arrow
+    p.triangle(
+      canvasWidth/2, canvasWidth - padding.y,
+      canvasWidth/2 + 5, canvasWidth - padding.y - 12,
+      canvasWidth/2 - 5, canvasWidth - padding.y - 12,
+    )
+    // left arrow
+    p.triangle(
+      padding.x, canvasWidth/2,
+      padding.x + 12, canvasWidth/2 + 5,
+      padding.x + 12, canvasWidth/2 - 5,
+    )
+    // right arrow
+    p.triangle(
+      canvasWidth - padding.x, canvasWidth/2,
+      canvasWidth - padding.x - 12, canvasWidth/2 + 5,
+      canvasWidth - padding.x - 12, canvasWidth/2 - 5,
+    )
+    // labels
+    drawLabels('neutral', canvasWidth/2, canvasWidth/2);
+    drawLabels('negative', canvasWidth/5, canvasWidth/2);
+    drawLabels('positive', canvasWidth*4/5, canvasWidth/2);
+    drawLabels('high', canvasWidth/2, canvasWidth/5);
+    drawLabels('low', canvasWidth/2, canvasWidth*4/5);
+    // selected position cursor
+    p.stroke(dark);
+    p.strokeWeight(strokeLight);
+    p.line(
+      position.x, position.y - cursorSize/2,
+      position.x, position.y + cursorSize/2,
     );
     p.line(
-      strokeBold/2 + padding.x + position + handleWidth, strokeLight/2 + padding.y + strokeBold/5, 
-      strokeBold/2 + padding.x + position + handleWidth, padding.y + strokeBold - strokeLight/2 - strokeBold/5
+      position.x - cursorSize/2, position.y,
+      position.x + cursorSize/2, position.y,
     );
+    // hints
+    if (enableHints) {
+      drawHints('AROUSAL', canvasWidth/2 + (smallScreen ? 35 : 50), padding.y + 8);
+      drawHints('VALENCE', canvasWidth - (smallScreen ? 20 : 35) - padding.x, canvasWidth/2 - (smallScreen ? 12 : 20));
+      let hintsCircleRadius = canvasWidth*3/8;
+      let angleTilt = Math.PI/12;
+      for(let i = 0; i < subjectives.length; i++) {
+        let word = subjectives[i];
+        let angle = Math.PI/6*i + angleTilt;
+        let x = canvasWidth/2 + Math.cos(angle)*hintsCircleRadius;
+        let y = canvasWidth/2 + Math.sin(angle)*hintsCircleRadius;
+        drawHints(word, x, y);
+      }
+    }
+    // draw help button
+    p.noStroke();
+    p.fill(lightBlue);
+    p.ellipse(canvasWidth - helpRadius/2 - padding.x, canvasHeight - helpRadius/2 - padding.y, helpRadius, helpRadius);
+    p.textSize(helpRadius - 20);
+    p.textAlign(p.CENTER, p.CENTER);
+    p.fill(enableHints ? dark : mediumGrey);
+    p.noStroke();
+    p.text('?', canvasWidth - helpRadius/2 - padding.x, canvasHeight - helpRadius/2 - padding.y);
   }
 };
 
-new p5(sketch, window.document.getElementById('force-input'));
+new p5(sketch, window.document.getElementById('valence-arousal'));
